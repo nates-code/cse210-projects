f9fb4430d76061e849841299e999605561c7af4d
nates-code <nathan@danablue.com>
2023-01-07 23:54:32 -0700

    Changed to give a letter grade based on grade persentage.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade persentage for this class? ");
+        string answer = Console.ReadLine();
+        int grade = System.Int32.Parse(answer);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }        
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Cangradulation you have passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You didn't pass the class. Better luck next time.");
+        }
     }
 }
