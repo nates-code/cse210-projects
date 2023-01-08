f3572c01d2d71d2858a0f8251d153c25428cb0f8
nates-code <nathan@danablue.com>
2023-01-07 22:45:15 -0700

    Changed the program to get name and say it back

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        Console.ReadLine();
+        string fname = Console.ReadLine();
+
+        Console.Write("What is you last name?");
+        Console.ReadLine();
+        string lname = Console.ReadLine();
+
+        Console.Write($"Your name is {lname}, {fname} {lname}.");
     }
 }
