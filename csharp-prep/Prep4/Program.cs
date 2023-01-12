7fa5a730082defcd556c83f5a23b55b94598287e
nates-code <nathan@danablue.com>
2023-01-11 21:18:23 -0700

    Made a the number thing.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,38 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<int> numList = new List<int>();
+        int number = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+                numList.Add(number);
+            }
+        }
+
+        int sum = numList.Sum();
+        double average = numList.Average();
+        int maxNum = numList.Max();
+
+        Console.WriteLine($"The sum is: {sum}"); 
+        Console.WriteLine($"The average is: {average}"); 
+        Console.WriteLine($"The largest number is: {maxNum}"); 
+
+        //Another way...
+        // foreach ()
+        // {
+            
+        // }
+        
+
+
     }
 }
