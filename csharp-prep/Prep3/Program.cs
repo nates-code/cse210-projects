cc443402aaf3bab41a1a659fce57c424b2ec95f4
nates-code <nathan@danablue.com>
2023-01-11 19:59:05 -0700

    Made a number guessing game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicInput = Console.ReadLine();
+        // int magicNumber = System.Int32.Parse(magicInput);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher ");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
