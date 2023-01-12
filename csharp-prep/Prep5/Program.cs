21f2738e14e587a481b9ca026807c75dfba8ffdd
nates-code <nathan@danablue.com>
2023-01-11 23:11:27 -0700

    Wrote fout functions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        {
+            DisplayWelcome();
+
+            string Username = PromptUserName();
+            int userNumber = PromptUserNumber(); 
+
+            int squaredNumber = SquareNumber(userNumber);
+
+            DisplayResult(Username, squaredNumber);
+
+        }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name; 
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite nember? ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square} ");
+        }
+
     }
 }
