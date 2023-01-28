cea42161b7ea6aedad03f7625e2090ce56fabcf8
nates-code <nathan@danablue.com>
2023-01-28 16:32:07 -0700

    Added a UI.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,10 +7,58 @@ class Program
 
     static void Main(string[] args) {
         
-    
-        Journal journal = new Journal();
-        journal.CreateEntry();
-        journal.DisplayEntries();
+        var userChoice = "";
+
+        while (userChoice != "5")
+        {
+            System.Console.WriteLine("Please select one of the following choices: ");
+            System.Console.WriteLine("1. Write");
+            System.Console.WriteLine("2. Display");
+            System.Console.WriteLine("3. Load");
+            System.Console.WriteLine("4. Save");
+            System.Console.WriteLine("5. Quit");
+            System.Console.Write("What would you like to do? ");
+            //int userChoice = int.Prarse(Console.ReadLine())
+            userChoice = Console.ReadLine();
+
+            if(int.TryParse(userChoice, out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        System.Console.WriteLine("1");
+
+                        Journal journal = new Journal();
+                        journal.CreateEntry();
+                        journal.DisplayEntries();
+
+                        break; 
+                    case 2:
+                        System.Console.WriteLine("2");
+                        break; 
+                    case 3:
+                        System.Console.WriteLine("3");
+                        break; 
+                    case 4:
+                        System.Console.WriteLine("4");
+                        break; 
+                    case 5:            
+                        System.Console.WriteLine("5");
+                        break; 
+                    default:
+                        System.Console.WriteLine("Defa");
+                        userChoice = "5";
+                        break;
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid input, please select a number between 1 and 5.");
+            }
+            
+        }
+
+        
     
 
         // public void LoadFile()
@@ -24,7 +72,7 @@ class Program
 
         // public void CreateNewJournal()  
         // {
-            void newjournal =  
+            // void newjournal =  
         // }
 
     }
