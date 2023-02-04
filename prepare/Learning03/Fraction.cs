75bcf461063b658424ca1fcb9c15ee870b08a368
nates-code <nathan@danablue.com>
2023-02-03 23:00:18 -0700

    Add file for Fraction class

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,28 @@
+using Systeml;
+
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+    
+   public Fraction()
+    {
+        private int fraction1 = 1/1;
+        System.Console.WriteLine(fraction1);
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        private var fraction2 = wholeNumber/1;
+        System.Console.WriteLine(fraction2);
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        private var fraction3 = top/bottom;
+        System.Console.WriteLine(fraction3);
+    }
+
+
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        
     }
 }
